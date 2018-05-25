d130d5390354102902f0e3332d140213866cba26
Nitish Dubey <astixset2@gmail.com>
2018-05-25 18:30:30 +0530

    CompetitorPrdctPriceActivity activity has been added which is a survey of Compertitor Product and 2 tables in database has been added with tblCompettrPrdctPTRPTC.

--- app/src/main/java/com/astix/Common/CommonInfo.java
+++ app/src/main/java/com/astix/Common/CommonInfo.java
@@ -316,8 +316,8 @@ public class CommonInfo
 
 	public static int AnyVisit = 0;
 
-	public static int DATABASE_VERSIONID = 76;      // put this field value based on value in table on the server
-	public static String AppVersionID = "1.20";   // put this field value based on value in table on the server
+	public static int DATABASE_VERSIONID = 79;      // put this field value based on value in table on the server
+	public static String AppVersionID = "1.21";   // put this field value based on value in table on the server
 	public static int Application_TypeID = 2; //1=Parag Store Mapping,2=Parag SFA Indirect,3=Parag SFA Direct
 
 	public static String OrderSyncPath="http://103.20.212.194/ReadXML_LTFoodsTestRelease/DefaultSFA.aspx";
